51a82c54cae908a444d104854702525343904318
tarazhong <zhong.tara@gmail.com>
2018-02-17 20:32:59 +0100

    Add Wall Animation And Succeed Canvas

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+/// <summary>
+/// Class responsible for GameOver and Success
+/// </summary>
+public class GameMaster : MonoBehaviour {
+
+    public GameObject retryUI;
+    public static bool retry = false; // for 1st loading, we don't want to display "retry" txt
+
+
+    // public GameObject finishUI;
+    public static float elapsedTime = 0; // we wan't to get the time spend on menu
+    public Transform player;
+    public Transform goal;
+    public TextMeshProUGUI timeFinish;
+    public GameObject finishUI;
+    private bool finish;
+
+    public static GameMaster gameOverinstance;
+    //SINGLETON
+    /// <summary>
+    /// Initialize singleton instance and variables
+    /// </summary> 
+    private void Awake()
+    {
+        Debug.Log("Time"+(Time.time - elapsedTime));
+        if (gameOverinstance != null)
+        {
+            Debug.LogError("More than one Player in scene");
+            return;
+        }
+        else
+        {
+            gameOverinstance = this;
+        }
+        finish = false;
+    }
+
+    /// <summary>
+    /// Function called when the object becomes enabled and active
+    /// </summary>
+    public void OnEnable()
+    {
+        retryUI.SetActive(GameMaster.retry); // Activate Retry Ui, Animation will play on Entry
+        MainCharacterController.OnWallCollision += Retry; // Subscribing to OnCollision event
+    }
+
+    /// <summary>
+    /// Function called when the object becomes disabled and inactive
+    /// </summary>
+    public void OnDisable()
+    {
+        MainCharacterController.OnWallCollision -= Retry;
+    }
+
+    /// <summary>
+    /// Function called when OnCollision broadcast a signal
+    /// </summary>
+    public void Retry()
+    {
+        SceneManager.LoadScene(1); // Reload 1st lvl
+        GameMaster.retry = true; // Tell the class that we have retried (so the next scene can display retry UI)
+    }
+
+    /// <summary>
+    /// Check if the player has reached goal
+    /// </summary>
+    private void Update()
+    {
+        // float time = Mathf.Floor(Time.time - elapsedTime);
+        // timeFinish.text = time.ToString();
+        // if the player press "Space" and is not moving and not swaping maps
+        if (Vector3.Distance(player.position,goal.position) <=0.5f&&!finish)
+        {
+            Debug.Log("Finish");
+            Debug.Log("Time" + (Time.time - elapsedTime));
+            finish = true;
+            finishUI.SetActive(true);
+            float time = Mathf.Floor(Time.time - elapsedTime);
+            timeFinish.text = time.ToString();
+        };
+    }
+}
--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -5,23 +5,24 @@ using UnityEngine;
 /// <summary>
 /// Class responsible for Map controlls :
 /// </summary>
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
 
     [Header("Maps variables")]
+    // Active map
+    public Transform[] tilesMap;
     #region map1
     public Transform map1; // TileList
     private Transform[] tilesMap1;
     private bool map1active;
     #endregion
-
     #region map2
     public Transform map2; // TileList
     private Transform[] tilesMap2;
     private bool map2active;
     #endregion
+    private bool mapSwap;
 
-    // Active map
-    public Transform[] tilesMap;
 
     public static MapManager mapInstance;
     //SINGLETON
@@ -44,7 +45,8 @@ public class MapManager : MonoBehaviour {
     /// <summary>
     /// Initialize variables
     /// </summary>
-    private void Start () {
+    private void Start()
+    {
 
         // Get child elements from map object
         tilesMap1 = new Transform[map1.childCount];
@@ -65,24 +67,46 @@ public class MapManager : MonoBehaviour {
         map1.gameObject.SetActive(map1active);
         map2.gameObject.SetActive(map2active);
         tilesMap = tilesMap1;
+        mapSwap = true;
     }
-    
+
     /// <summary>
     /// Get User input for switching Map
     /// </summary>
-    private void Update () {
-        if (Input.GetKeyDown(KeyCode.Space)&&!MainCharacterController.characterController.animator.GetBool("isWalking")) // if the player is not moving
+    private void Update()
+    {
+        // if the player press "Space" and is not moving and not swaping maps
+        if (Input.GetKeyDown(KeyCode.Space) && !MainCharacterController.characterController.animator.GetBool("isWalking") && mapSwap) 
         {
-            map1active = !map1active;
-            map2active = !map2active;
-            map1.gameObject.SetActive(map1active);
-            map2.gameObject.SetActive(map2active);
-            if (map1.gameObject.activeInHierarchy)
-                tilesMap = tilesMap1;
-            else
-                tilesMap = tilesMap2;
+            IEnumerator coroutine = SwapMaps();
+            StartCoroutine(coroutine);
         }
-	}
+    }
+
+    /// <summary>
+    /// Function that swaps maps and play wall animation
+    /// </summary>
+    /// <returns>Return true if a wall with the given X and Z postion or false if not</returns>
+    private IEnumerator SwapMaps()
+    {
+        map1active = !map1active;
+        map2active = !map2active;
+        mapSwap = false;
+        map1.gameObject.SetActive(map1active);
+        map2.gameObject.SetActive(map2active);
+        if (map1.gameObject.activeInHierarchy)
+            tilesMap = tilesMap1;
+        else
+            tilesMap = tilesMap2;
+        foreach (Transform tile in tilesMap)
+        {
+            tile.position += new Vector3(0, 5, 0);
+        }
+        yield return new WaitForSeconds(1f);
+        #region WaitAndDo // this will be executed only when the coroutine have finished
+        mapSwap = true;
+        #endregion
+    }
 
     /// <summary>
     /// Chack if a wall exists in the active map with a given "x" and "y" position
@@ -92,12 +116,12 @@ public class MapManager : MonoBehaviour {
     /// <returns>Return true if a wall with the given X and Z postion or false if not</returns>
     public bool GetWall(float x, float z)
     {
-        foreach (Transform go in tilesMap)
+        foreach (Transform tile in tilesMap)
         {
-            TileBlock tile = go.GetComponent<TileBlock>();
-            if (tile!= null)
+            TileBlock tileBlock = tile.GetComponent<TileBlock>();
+            if (tileBlock != null)
             {
-                if(tile.x == x && tile.z == z)
+                if (tileBlock.x == x && tileBlock.z == z)
                 {
                     return true;
                 }
